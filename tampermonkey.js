548648d4c6c629af1b2b55fd9ad066c2a28aba68
NiuBen <niuben@lianlianpay.com>
2023-08-28 14:00:18 +0800

    Feat 初始化篡改猴脚本

---
+++ tampermonkey.js
@@ -0,0 +1,48 @@
+// ==UserScript==
+// @name         私人用通用脚本。
+// @namespace    all-round
+// @version      1.0.0 
+// @description  私人用的脚本，各种各样的功能。
+// @author       You
+// @match        https://www.google.com/*
+// @match        https://mapgenie.io/*
+// @includ       https://www.google.com/*
+// @includ        https://mapgenie.io/*
+// @grant        none
+// @author       Korey
+// @license MIT
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+    // 删除谷歌广告
+    function deleteGoogleAds() {
+        if (window.location.hostname.includes('google.com')) {
+            console.log('deleteGoogleAds');
+            const adsElement = document.getElementById("tads");
+            if (adsElement) {
+                adsElement.remove();
+            }
+        }
+    }
+
+    function deleteMapGenieAdsFloating() {
+        if (window.location.hostname.includes('mapgenie.io')) {
+            console.log('deleteMapGenieAdsFloating');
+            const adsElement = document.getElementById("blobby-left");
+            if (adsElement) {
+                adsElement.remove();
+            }
+        }
+    }
+
+    function mainFun() {
+        // 删除谷歌广告
+        deleteGoogleAds();
+        // 删除MapGenie广告
+        deleteMapGenieAdsFloating();
+    }
+
+    mainFun();
+})();
